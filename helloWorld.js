332975c77bf0d5fbd5f15c56210b9b5dee63289c
Xuan Mai <xmai@adobe.com>
2017-02-02 13:32:59 -0700

    nodejs helloworld

---
+++ helloWorld.js
@@ -0,0 +1,20 @@
+class TestOne {
+    /**
+     * @param {string} text Text to display
+     */
+    constructor(text) {
+        this.text = text;
+    }
+
+    /**
+     * @param {string} text Text to set
+     */
+    setText(text) {
+        this.text = text;
+    }
+
+    displayText() {
+        return this.text;
+    }
+}
+
